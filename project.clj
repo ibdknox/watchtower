efb8bc6a1bbec2abb47fec068842175f9129c4d4
Masashi Iizuka <liquidz.uo@gmail.com>
2012-04-14 15:52:39 +0900

    change name, and push to clojars

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
-(defproject watchtower "0.1.1"
-  :description "A library for directory watchers"
+(defproject
+  uochan/watchtower "0.1.2"
+  :description "A library for directory watchers forked by ibdknox/watchtower"
   :dependencies [[org.clojure/clojure "1.3.0"]])
